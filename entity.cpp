d4d2b1efa9ca4c2a7ab91d9b4d4fe220436abc4a
Ria Triantafillou <63511352+Ria-T@users.noreply.github.com>
2022-12-15 21:18:35 +0200

    Update entity.cpp

--- entity.cpp
+++ entity.cpp
@@ -2,44 +2,40 @@
 #include <iostream>
 using namespace std;
 
-class Entity 
-{
-  public:
+
     // Constructor
-    Entity(int health) : the_health(health) 
+    Entity::Entity(int health) : the_health(health) 
     {
         rand_Attack();
         rand_Defense();
     }
 
     // Destructor
-    virtual ~Entity() {}
+    virtual Entity::~Entity() {}
 
     // Getters
-    int getHealth() const { return the_health; }
-    int getAttack() const { return the_attack; }
-    int getDefense() const { return the_defense; }
+    int Entity::getHealth() const { return the_health; }
+    int Entity::getAttack() const { return the_attack; }
+    int Entity::getDefense() const { return the_defense; }
 
     // Setters
-    void setHealth(int health) { the_health = health; }
+    void Entity::setHealth(int health) { the_health = health; }
 
     // Function for generating a random attack value
-    void rand_Attack() 
+    void Entity::rand_Attack() 
     {
       the_attack = 1 + rand() % 3;
     }
 
     // Function for generating a random defense value
-    void rand_Defense() 
+    void Entity::rand_Defense() 
     {
       the_defense = 1 + rand() % 2;
     }
 
     // Virtual functions
-    virtual void defend(int damage) = 0;
-    virtual void attack(Entity& target) = 0;
-    virtual void move() = 0;
+    virtual void Entity::defend(int damage) = 0;
+    virtual void Entity::attack(Entity& target) = 0;
+    virtual void Entity::move() = 0;
 
-  protected:
     int the_health, the_attack, the_defense; // Entity's stats
-};
